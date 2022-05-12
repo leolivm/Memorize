0c25ed588e2f5319fbf122848b6932d114b7a090
leolivm <leolivm@outlook.com>
2022-05-12 02:37:25 -0300

    finishing the first class 2021

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,30 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            ForEach(0..<4) { index in
-                CardView(isFaceUp: false)
-            }
-        }
-        .padding()
-        .foregroundColor(Color.orange)
-        .font(Font.largeTitle)
-    }
-}
-
-struct CardView: View {
-    var isFaceUp: Bool
-
-    var body: some View {
         ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text("👻")
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
-            }
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello World").foregroundColor(.orange)
         }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
